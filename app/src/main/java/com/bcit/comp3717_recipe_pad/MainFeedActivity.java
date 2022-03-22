b965687a9ac8246af44b0a0b0860d0fa3793b1a5
KHuangbcit <81665366+KHuangbcit@users.noreply.github.com>
2022-03-21 17:12:06 -0700

    bottom nav bar

--- app/src/main/java/com/bcit/comp3717_recipe_pad/MainFeedActivity.java
+++ app/src/main/java/com/bcit/comp3717_recipe_pad/MainFeedActivity.java
@@ -1,8 +1,14 @@
 package com.bcit.comp3717_recipe_pad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainFeedActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainFeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView_mainfeed);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                return true;
+            }
+        });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home)
+        {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
