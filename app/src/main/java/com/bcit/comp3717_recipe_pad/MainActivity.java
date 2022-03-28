1dc2d9089486682ac7a99e70f599b60d808ce5a0
kazma-ku <77481133+kazma-ku@users.noreply.github.com>
2022-03-27 18:50:19 -0700

    created sign in function in MainActivity.java

--- app/src/main/java/com/bcit/comp3717_recipe_pad/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717_recipe_pad/MainActivity.java
@@ -1,17 +1,56 @@
 package com.bcit.comp3717_recipe_pad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            currentUser.reload();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editText_main_username);
+        password = findViewById(R.id.editText_main_password);
+        login = findViewById(R.id.button_main_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_mail = email.getText().toString();
+                String user_password = password.getText().toString();
+                signIn(user_mail, user_password);
+            }
+        });
     }
 
     public void signUp(View v) {
@@ -19,5 +58,15 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(MainActivity.this, SignupActivity.class);
         MainActivity.this.startActivity(myIntent);
 
+    }
+
+    public void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, MainFeedActivity.class));
+            }
+        });
     }
 }
