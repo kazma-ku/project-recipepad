8ea2c3a40479c6b1d165b367d1231775c508f29f
s-lando <slandolando@hotmail.com>
2022-04-03 23:33:57 -0700

    Added back code for trending feed onClick in TrendingRecyclerActivity

--- app/src/main/java/com/bcit/comp3717_recipe_pad/TrendingRecyclerActivity.java
+++ app/src/main/java/com/bcit/comp3717_recipe_pad/TrendingRecyclerActivity.java
@@ -1,5 +1,6 @@
 package com.bcit.comp3717_recipe_pad;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +26,7 @@ import com.google.firebase.storage.StorageReference;
 
 import java.time.Instant;
 import java.time.temporal.TemporalAdjuster;
+import java.util.ArrayList;
 
 
 public class TrendingRecyclerActivity extends RecyclerView.Adapter<TrendingRecyclerActivity.ViewHolder> {
@@ -42,6 +45,7 @@ public class TrendingRecyclerActivity extends RecyclerView.Adapter<TrendingRecyc
         private final TextView likes;
         private final TextView dislkes;
         private final TextView comments;
+        private final LinearLayout trendingFeed;
 
         public ViewHolder(View view) {
             super(view);
@@ -52,6 +56,7 @@ public class TrendingRecyclerActivity extends RecyclerView.Adapter<TrendingRecyc
             likes = view.findViewById(R.id.textView_item_likeCount);
             dislkes = view.findViewById(R.id.textView_item_dislikeCount);
             comments = view.findViewById(R.id.textView_item_commentCount);
+            trendingFeed = view.findViewById(R.id.linearlayout_trending_feed);
         }
 
         public TextView getTitle() {
@@ -62,32 +67,9 @@ public class TrendingRecyclerActivity extends RecyclerView.Adapter<TrendingRecyc
         public TextView getLikes() { return likes;}
         public TextView getDislkes() { return dislkes;}
         public TextView getComments() { return comments;}
+        public LinearLayout getTrendingFeed() { return trendingFeed;}
 
     }
-//
-//    public void getImage(){
-//        FirebaseStorage storage = FirebaseStorage.getInstance();
-//        StorageReference storageRef = storage.getReference();
-//        StorageReference imagesRef = storageRef.child(getImage(););
-//
-//        final long ONE_MEGABYTE = 1024 * 1024;
-//        imagesRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-//            @Override
-//            public void onSuccess(byte[] bytes) {
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//                // Handle any errors
-//            }
-//        });
-//
-//
-//
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        db.collection("recipes").document();
-//
-//    }
 
     /**
      * Initialize the dataset of the Adapter.
@@ -133,6 +115,31 @@ public class TrendingRecyclerActivity extends RecyclerView.Adapter<TrendingRecyc
         viewHolder.getLikes().setText(String.valueOf(recipes[position].getLikesNum()));
         viewHolder.getDislkes().setText(String.valueOf(recipes[position].getDislikesNum()));
         viewHolder.getComments().setText(String.valueOf(recipes[position].getCommentsNum()));
+
+        viewHolder.trendingFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Recipe recipe = recipes[viewHolder.getAdapterPosition()];
+
+                ArrayList<String> recipeInfo = new ArrayList<>();
+                recipeInfo.add(recipe.getTitle());
+                recipeInfo.add(String.valueOf(recipe.getLikesNum()));
+                recipeInfo.add(String.valueOf(recipe.getDislikesNum()));
+                recipeInfo.add(String.valueOf(recipe.getCommentsNum()));
+                recipeInfo.add(recipe.getDesc());
+                recipeInfo.add(recipe.getIngredients());
+                recipeInfo.add(recipe.getSteps());
+
+                Intent intent = new Intent(view.getContext(), RecipeActivity.class);
+
+                intent.putStringArrayListExtra("recipe", recipeInfo);
+                intent.putStringArrayListExtra("comments", recipe.getComments());
+                intent.putExtra("recipeID", recipe.getRecipeID());
+                intent.putExtra("userID", recipe.getUserID());
+
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     //
